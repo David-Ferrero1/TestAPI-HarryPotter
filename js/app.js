dd07e27034349b068bb1d456e7a97557190ef49f
David Ferrero <ferrero_d@hotmail.com>
2024-07-25 16:45:49 +0200

    fetch + création card en css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 const apiURL = "https://harry-potter-api-en.onrender.com/db"
 
 fetch(apiURL).then((res) => res.json()).then((data) => {
-    console.log(data);
+    //console.log(data);
+    data.characters.forEach(element => 
+        document.getElementById("main").innerHTML +=
+            '<div id="character">'+
+            '<img src= "' + element.image + '" width="150px" height="200px">' +
+            '<h2 id="name">' + element.character + '</h2>' +
+            '<div id="real-name">(' + element.interpretedBy + ')</div>' +
+            '<div id="house">' + element.hogwartsHouse + '</div>' +
+            '</div>'
+    );
 })
+// const apiURL = "https://hp-api.onrender.com/api/characters"
+
+// fetch(apiURL).then((res) => res.json()).then((data) => {
+//     //console.log(data);
+//     data.forEach(element => 
+//         document.getElementById("main").innerHTML +=
+//             '<div id="character">'+
+//             '<img src= "' + element.image + '" width="150px" height="200px">' +
+//             '<div id="name">' + element.name + '</div>' +
+//             '<div id="house">' + element.house + '</div>' +
+//             '<div id="house">' + element.house + '</div>' +
+//             '</div>'
+//     );
+// })
